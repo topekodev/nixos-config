8787c06d30ce70a853b8307dbc890b90b9a4ead9
topekodev <tomi@topeko.dev>
2024-07-12 05:00:13 +0300

    Update disko-config

--- disko-config.nix
+++ disko-config.nix
@@ -25,12 +25,14 @@
                                 type = "luks";
                                 name = "crypt";
                                 settings = {
+                                    keyFile = "/dev/mapper/cryptkey";
+                                    keyFileSize = 8192;
                                     allowDiscards = true;
                                 };
                                 postCreateHook = ''
-                                    dd bs=512 count=4 iflag=fullblock if=/dev/random of=/mnt/crypt.key
-                                    chmod 600 /mnt/crypt.key
-                                    cryptsetup luksAddKey /dev/sda2 /mnt/crypt.key /tmp/disk.key
+                                    dd bs=512 count=4 iflag=fullblock if=/dev/random of=/tmp/crypt.key
+                                    chmod 600 /tmp/crypt.key
+                                    cryptsetup luksAddKey --key-file /dev/mapper/cryptkey --keyfile-size 8192 /dev/sda2 /tmp/crypt.key
                                 '';
                                 content = {
                                     type = "btrfs";
