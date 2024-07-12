073db19a76583c7152b7e60fa6d6e133524ce008
topekodev <tomi@topeko.dev>
2024-07-12 17:25:46 +0300

    Removed sudo

--- install.sh
+++ install.sh
@@ -24,4 +24,4 @@ echo $password > /tmp/secret.key
 
 confirm "Partition $device"
 cp ./disko-config.nix /tmp/disko-config.nix
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix --arg device '"'$device'"'
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix --arg device '"'$device'"'
